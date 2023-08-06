34c2c75715be06b41dea90f83851b38cec4876c5
Joseph Webster <rwc.webster@gmail.com>
2023-08-06 05:19:10 +0100

    Refactored web crawler script for improved error handling and logging. Updated URL parsing and validation methods. Optimized crawling process to avoid redundant requests.

--- web_crawler_oop_recursive.py
+++ web_crawler_oop_recursive.py
@@ -2,40 +2,40 @@ import requests
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 import re
+import os
+import logging
 
 class Crawler:
     def __init__(self, base_url):
         self.internal_urls = set()
         self.external_urls = set()
         self.base_url = base_url
-        self.clean_url = base_url.replace("https://", "").replace("http://", "")
+        self.clean_url = urlparse(base_url).netloc
+        self.session = requests.Session()
 
     def is_valid(self, url):
         parsed = urlparse(url)
         return bool(parsed.netloc) and bool(parsed.scheme)
 
     def verify_word_in_url(self, word, url):
-        pattern = r"(https?://)(\w+\.)+\w+(/\S*)?"
-        match = re.search(pattern, url)
-        if match:
-            url_without_protocol = match.group(0)[len(match.group(1)):]
-            domain = url_without_protocol.split('/')[0]
-            return word in domain
-        return False
+        domain = urlparse(url).netloc
+        return word in domain
     
     def classify(self, url):
         base_domain = self.clean_url
-        tld = re.search(r"\.[a-zA-Z]{2,}.*$", base_domain).group()
-        base_domain = base_domain.replace(tld, "")
         if self.verify_word_in_url(base_domain, url):
             self.internal_urls.add(url)
         else:
             self.external_urls.add(url)
 
     def crawl(self, url):
-        response = requests.get(url)
+        try:
+            response = self.session.get(url)
+        except requests.RequestException:
+            logging.error(f"Failed to get {url}")
+            return
         if url.endswith('.xml'):
-            soup = BeautifulSoup(response.content, 'lxml-xml')  # need to run pip install lxml for this to work 
+            soup = BeautifulSoup(response.content, 'lxml-xml')
         else:
             soup = BeautifulSoup(response.text, 'html.parser')
         for a_tag in soup.findAll("a"):
@@ -43,8 +43,6 @@ class Crawler:
             if href == "" or href is None:
                 continue
             href = urljoin(url, href)
-            parsed_href = urlparse(href)
-            href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
             if not self.is_valid(href):
                 continue
             self.classify(href)
@@ -52,14 +50,11 @@ class Crawler:
     def crawl_all(self):
         self.crawl(self.base_url)
         for link in list(self.internal_urls):
-            self.crawl(link)
+            if link not in self.external_urls:
+                self.crawl(link)
 
     def save(self):
-        if "/" in self.clean_url:
-            save_url = self.clean_url.replace("/", "-")
-        else:
-            save_url = self.clean_url
-
+        save_url = self.clean_url.replace("/", "-")
         if len(self.internal_urls) != 0:
             with open(f"internal_{save_url}.txt", "w") as file:
                 for url in self.internal_urls:
@@ -71,8 +66,9 @@ class Crawler:
                     file.write(url + "\n")
 
 if __name__ == '__main__':
-    crawler = Crawler("https://example.org")
+    logging.basicConfig(level=logging.INFO)
+    crawler = Crawler("https://example.com/")
     crawler.crawl_all()
     crawler.save()
-    print("Total Internal links:", len(crawler.internal_urls))
-    print("Total External links:", len(crawler.external_urls))
+    logging.info("Total Internal links: %s", len(crawler.internal_urls))
+    logging.info("Total External links: %s", len(crawler.external_urls))
